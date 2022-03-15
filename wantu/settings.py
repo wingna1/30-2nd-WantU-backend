1bab3cef71f6e5002d492b9ad9582dffdfe8688a
Park YengSeo <1park4170@gmail.com>
2022-03-15 16:25:17 +0900

    model migrated

--- wantu/settings.py
+++ wantu/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'applications',
+    'cv',
+    'jobs',
+    'users'
 ]
 
 MIDDLEWARE = [
