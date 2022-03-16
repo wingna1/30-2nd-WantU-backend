9622768ac283746afcfeaad40c498e1c15916bf3
lesol <wingnawing@gmail.com>
2022-03-16 11:48:14 +0900

    Modify: Modeling

--- cv/models.py
+++ cv/models.py
@@ -4,9 +4,9 @@ from utils.time_stamp_model import TimeStampModel
 from users.models           import User
 
 class Resume(TimeStampModel):
-    name    = models.CharField(max_length=30)
-    pdf_url = models.URLField(max_length=1500)
-    user    = models.ForeignKey(User, on_delete= models.CASCADE)
+    name     = models.CharField(max_length=30)
+    file_url = models.URLField(max_length=1500)
+    user     = models.ForeignKey(User, on_delete= models.CASCADE)
 
     class Meta:
         db_table = "resumes"
--- users/models.py
+++ users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from utils.time_stamp_model import TimeStampModel
 
 class User(TimeStampModel):
-    name                = models.CharField(max_length=20)
-    kakao_id            = models.BigIntegerField(unique=True)
-    profile_image_url   = models.URLField(max_length=200)
+    kakao_nickname    = models.CharField(max_length=20)
+    kakao_id          = models.BigIntegerField(unique=True)
+    profile_image_url = models.URLField(max_length=1500)
 
     class Meta:
         db_table = "users"
