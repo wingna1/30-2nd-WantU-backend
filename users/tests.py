6aab51d8eaf3fc988fa4b641475375375123e4e6
lesol <wingnawing@gmail.com>
2022-03-17 16:35:07 +0900

    Add: users app >> test.py kakaoLoginTest

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,156 @@
-from django.test import TestCase
+import jwt
 
-# Create your tests here.
+from django.test    import Client, TestCase
+from unittest.mock  import MagicMock, patch
+
+from datetime       import datetime, timedelta
+from my_settings    import SECRET_KEY, ALGORITHM
+from users.models   import User
+
+class KakaoLoginTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        User.objects.create(
+            id                = 1,
+            kakao_nickname    = "핑핑이",
+            kakao_email       = "pingping@gmail.com",
+            kakao_id          = 1234567,
+            profile_image_url = "http://111.jpg"
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch("users.views.requests")
+    def test_login_success_for_user(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id":1234567,
+                    "properties":{
+                        "profile_image":"http://111.jpg",
+                        "nickname"     :"핑핑이"
+                    },
+                    "kakao_account":{
+                        "email":"pingping@gmail.com"
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        headers  = {"HTTP_access_token":"fakeTokenforTest"}
+        response = client.get("/users/kakao/login", **headers)
+
+        response_token = response.json()["results"]["token"]
+        payload        = jwt.decode(response_token, SECRET_KEY, ALGORITHM)  
+        token          = jwt.encode({"id":payload["id"], "exp":datetime.utcnow() + timedelta(days=2)}, SECRET_KEY, ALGORITHM)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "results":{
+                "token"            :token,
+                "kakao_email"      :"pingping@gmail.com",
+                "kakao_nickname"   :"핑핑이",
+                "profile_image_url":"http://111.jpg"
+            }
+        })
+
+    @patch("users.views.requests")
+    def test_fail_for_absence_of_kakao_token(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id":1234567,
+                    "properties":{
+                        "profile_image":"http://111.jpg",
+                        "nickname"     :"핑핑이"
+                    },
+                    "kakao_account":{
+                        "email":"pingping@gmail.com"
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        response = client.get("/users/kakao/login")
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message":"Invalid Access Token"})
+
+    @patch("users.views.requests")
+    def test_login_fail_for_invalid_user_information_from_kakao(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "properties":{
+                        "profile_image":"http://111.jpg"
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        
+        headers  = {"HTTP_access_token":"fakeTokenforTest"}
+        response = client.get("/users/kakao/login", **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message":"Key Error"})
+
+    @patch("users.views.requests")
+    def test_login_fail_for_time_out(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id":1234567,
+                    "properties":{
+                        "profile_image":"http://111.jpg",
+                        "nickname"     :"핑핑이"
+                    },
+                    "kakao_account":{
+                        "email":"pingping@gmail.com"
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+        
+        headers  = {"HTTP_access_token":"fakeTokenforTest"}
+        response = client.get("/users/kakao/login", **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message":"Time Out"})
+
+    @patch("users.views.requests")
+    def test_update_success_for_existing_user(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id":1234567,
+                    "properties":{
+                        "profile_image":"http://1313.jpg",
+                        "nickname"     :"세젤귀 핑핑"
+                    },
+                    "kakao_account":{
+                        "email":"pingping@gmail.com"
+                    }
+                }
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        headers  = {"HTTP_access_token":"fakeTokenforTest"}
+        response = client.get("/users/kakao/login", **headers)
+
+        response_token = response.json()["results"]["token"]
+        payload        = jwt.decode(response_token, SECRET_KEY, ALGORITHM)  
+        token          = jwt.encode({"id":payload["id"], "exp":datetime.utcnow() + timedelta(days=2)}, SECRET_KEY, ALGORITHM)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "results":{
+                "token"            :token,
+                "kakao_email"      :"pingping@gmail.com",
+                "kakao_nickname"   :"세젤귀 핑핑",
+                "profile_image_url":"http://1313.jpg"
+            }
+        })
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 import requests, jwt
 
-from datetime         import datetime, timedelta
-from django.http      import JsonResponse
-from django.views     import View
+from datetime               import datetime, timedelta
+from django.http            import JsonResponse
+from django.views           import View
+from requests.exceptions    import Timeout
 
 from my_settings      import SECRET_KEY, ALGORITHM
 from users.models     import User
@@ -10,33 +11,35 @@ from users.models     import User
 class KakaoLoginView(View):
     def get(self, request):
         try:
-            access_token = request.headers.get("access_token", None)
+            access_token = request.headers.get("Authorization", None)
 
             if access_token == None:
                 return JsonResponse({"message":"Invalid Access Token"}, status=401)
 
             kakao_user_api   = "https://kapi.kakao.com/v2/user/me"
             headers          = {"Authorization":f"Bearer ${access_token}"}
-            user_information = requests.get(kakao_user_api, headers=headers, timeout=6).json()
+            user_information = requests.get(kakao_user_api, headers=headers, timeout=5).json()
 
             kakao_id          = user_information["id"]
+            kakao_email       = user_information["kakao_account"]["email"]
             profile_image_url = user_information["properties"]["profile_image"]
             kakao_nickname    = user_information["properties"]["nickname"]
 
             user, created = User.objects.update_or_create( 
                 kakao_id = kakao_id, 
-                defaults = {"kakao_nickname":kakao_nickname, "profile_image_url":profile_image_url}
+                defaults = {"kakao_nickname":kakao_nickname, "profile_image_url":profile_image_url, "kakao_email":kakao_email}
             )
 
             token = jwt.encode({"id":user.id, "exp":datetime.utcnow() + timedelta(days=2)}, SECRET_KEY, ALGORITHM)
 
             results = {
                 "token"            :token,
+                "kakao_email"      :user.kakao_email,
                 "kakao_nickname"   :user.kakao_nickname,
                 "profile_image_url":user.profile_image_url
             }
             return JsonResponse({"results":results}, status=200)
-        except requests.exceptions.Timeout:
+        except Timeout:
             return JsonResponse({"message":"Time Out"}, status=408)
         except KeyError:
             return JsonResponse({"message":"Key Error"}, status=400)
