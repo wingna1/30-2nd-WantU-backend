01661a44d1dc0c5bcac0407de03bc59c41d56fdb
Park YengSeo <1park4170@gmail.com>
2022-03-21 15:53:57 +0900

    add is_deleted, deleted_at

--- cv/models.py
+++ cv/models.py
@@ -1,15 +1,17 @@
 import uuid
 
 from django.db import models
+from datetime import datetime
 
 from utils.time_stamp_model import TimeStampModel
 from users.models           import User
 
 class Resume(TimeStampModel):
-    name     = models.CharField(max_length=30)
-    file_url = models.URLField(max_length=1500)
-    uuid     = models.UUIDField(primary_key=False , default=uuid.uuid4, editable=False)
-    user     = models.ForeignKey(User, on_delete= models.CASCADE)
-
+    name       = models.CharField(max_length=30)
+    file_url   = models.URLField(max_length=1500)
+    uuid       = models.UUIDField(primary_key=False , default=uuid.uuid4, editable=False)
+    user       = models.ForeignKey(User, on_delete= models.CASCADE)
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True)
     class Meta:
         db_table = "resumes"
